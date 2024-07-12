464add4c273b57f88fd2e487f71afa3bf02b984d
Luckypanthersohil <luckysjlucky@gmail.com>
2024-07-12 23:16:33 +0530

    third commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+public class Main9 {
+
+    public static void main(String[] a) {
+        int num[][] = {{1, 2, 3, 4}, {2, 3, 4, 5}, {4, 5, 6, 7}};;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(num[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
